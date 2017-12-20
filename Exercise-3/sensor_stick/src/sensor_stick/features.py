3dd1530f66a6db18a426839078f1defd899a2a0c
Lincoln Stein <lxiscs@gmail.com>
2017-12-20 08:22:54 -0800

    Code updated for each exercise

--- Exercise-3/sensor_stick/src/sensor_stick/features.py
+++ Exercise-3/sensor_stick/src/sensor_stick/features.py
@@ -10,7 +10,7 @@ def rgb_to_hsv(rgb_list):
     return hsv_normalized
 
 
-def compute_color_histograms(cloud, using_hsv=False):
+def compute_color_histograms(cloud, using_hsv=True):
 
     # Compute histograms for the clusters
     point_colors_list = []
@@ -33,13 +33,16 @@ def compute_color_histograms(cloud, using_hsv=False):
         channel_2_vals.append(color[1])
         channel_3_vals.append(color[2])
     
+
     # TODO: Compute histograms
-
+    h_hist = np.histogram(channel_1_vals, bins=32, range=(0, 256))
+    s_hist = np.histogram(channel_2_vals, bins=32, range=(0, 256))
+    v_hist = np.histogram(channel_3_vals, bins=32, range=(0, 256))
     # TODO: Concatenate and normalize the histograms
-
+    hist_channel_features = np.concatenate((h_hist[0], s_hist[0], v_hist[0])).astype(np.float64)
     # Generate random features for demo mode.  
     # Replace normed_features with your feature vector
-    normed_features = np.random.random(96) 
+    normed_features = hist_channel_features / np.sum(hist_channel_features)
     return normed_features 
 
 
@@ -56,11 +59,13 @@ def compute_normal_histograms(normal_cloud):
         norm_z_vals.append(norm_component[2])
 
     # TODO: Compute histograms of normal values (just like with color)
-
+    x_hist = np.histogram(norm_x_vals, bins=32, range=(0, 256))
+    y_hist = np.histogram(norm_y_vals, bins=32, range=(0, 256))
+    z_hist = np.histogram(norm_z_vals, bins=32, range=(0, 256))
     # TODO: Concatenate and normalize the histograms
-
+    hist_norm_features = np.concatenate((x_hist[0], y_hist[0], z_hist[0])).astype(np.float64)
     # Generate random features for demo mode.  
     # Replace normed_features with your feature vector
-    normed_features = np.random.random(96)
+    normed_features = hist_norm_features / np.sum(hist_norm_features)
 
     return normed_features
